895025e2aa41c18e7e84497b6fb335683b3c2ef8
javad tousi <91008279+javad-tousi@users.noreply.github.com>
2021-09-25 14:49:46 +0330

    Add files via upload

---
+++ GAM.App/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GAM")]
+[assembly: AssemblyDescription("سامانه گزارشات اعتباری و مالی")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bank Mellat")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("by Javad Tousi")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("63c71d8d-1d4c-484e-9953-a8bca291951e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("11.97.8.8")]
+[assembly: AssemblyFileVersion("11.97.8.8")]
+[assembly: NeutralResourcesLanguage("fa")]
+
